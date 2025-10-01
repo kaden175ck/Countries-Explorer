401ed5deafcddb7e1b3435d770220b2182b8eea0
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2025-10-01 23:26:58 +0800

    create nav bar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() { 
+    return (
+        <div>
+            <div>
+                <div>
+                    <span>
+                        Countries<span>Explorer</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
