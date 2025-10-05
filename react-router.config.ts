91e10c8eaacd1705d02630fb6497a304e717b984
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2025-10-05 17:09:03 +0800

    chore: setup GitHub Pages deployment

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+
+  basename: "/Countries-Explorer/",
   ssr: true,
 } satisfies Config;
