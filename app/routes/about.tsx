78f281a8422c8e00ec8b51684de654ec50187575
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2025-10-05 12:20:36 +0800

    render about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,22 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-16 bg-white">
+      <div className="container mx-auto px-4 max-w-4xl">
+        <h1 className="text-4xl font-extrabold text-gray-900 mb-6 text-center">About CountriesExplorer</h1>
+        <p className="text-xl text-gray-700 leading-relaxed mb-4">
+          This website uses the <span className="font-semibold text-indigo-600"> REST Countries API </span>
+          to display comprehensive information about countries worldwide.
+        </p>
+        <p className="text-lg text-gray-700 leading-relaxed mb-4">
+            Explore our data to learn about country names, capitals, regions,
+            populations, flags, and much more. Whether you’re curious about a
+            particular country or looking for insights about global regions, our
+            interactive explorer makes it easy.
+        </p>
+        <p className="text-lg text-gray-700 leading-relaxed">
+          We hope you find CountriesExplorer informative and engaging!
+        </p>
+      </div>
+    </div>
+  )
 }
