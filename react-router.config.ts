70758aa44589778a4def2897aebaf7e7b2597c1d
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2025-10-06 14:10:09 +0800

    add comments

--- react-router.config.ts
+++ react-router.config.ts
@@ -7,3 +7,8 @@ export default {
   basename: "/Countries-Explorer/",
   ssr: false,
 } satisfies Config;
+// 是否开启 SSR（服务端渲染）
+  // - true = 使用 SSR，适合有服务器的部署（如 Vercel、Node 服务器）
+  // - false = 使用 SPA（单页应用），适合静态托管（如 GitHub Pages）
+  // ⚠️ GitHub Pages 是纯静态的，没有服务器，
+  // 所以必须设置为 false，否则页面刷新会 404。
